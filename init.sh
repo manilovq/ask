ea5791c416d89fb33213cdfd7ff65741c61f5d3a
manilovq <manilovq@gmail.com>
2017-10-03 17:59:58 +0300

    заданиe №1

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web1/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
