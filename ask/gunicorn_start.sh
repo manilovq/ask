baf6eb9052ae2e566777507cb48d8e876ae72224
manilovq <manilovq@gmail.com>
2017-10-04 17:02:32 +0300

    начал работу с моделью

--- ask/gunicorn_start.sh
+++ ask/gunicorn_start.sh
@@ -1,1 +1,2 @@
+#!/bin/sh 
 sudo gunicorn -w 1 -b 0.0.0.0:8000 ask.wsgi:application
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField(blank=True)
+    rating = models.IntegerField()
+    author = models.ManyToManyField(User)
+    likes = User
+
+    def __unicode__(self):
+        return self.title
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField()
+    question = models.ForeignKey(Question)
+    author = models.CharField(max_length=50)
