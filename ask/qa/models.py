2501fe21efdfb2631664b28152c21bbb5d48295b
manilovq <manilovq@gmail.com>
2017-10-16 11:48:43 +0300

    fix field

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -17,8 +17,8 @@ class Question(models.Model):
     text = models.TextField()
     added_at = models.DateField(blank=True, auto_now_add=True)
     rating = models.IntegerField(default=0)
-    author = models.OneToOneField(User)
-    likes = models.ManyToManyField(User, related_name='likes_set')
+    author = models.ForeignKey(User, null=False)
+    likes = models.ManyToManyField(User, related_name='likes_set', blank=True)
 
     def __unicode__(self):
         return self.title
@@ -26,6 +26,6 @@ class Question(models.Model):
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateField()
+    added_at = models.DateField(auto_now_add=True)
     question = models.ForeignKey(Question)
-    author = models.CharField(max_length=50)
+    author = models.ForeignKey(User)
