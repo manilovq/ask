9a071872cd3c553bfb8c2266c296852f51c5327f
manilovq <manilovq@gmail.com>
2017-10-16 11:27:50 +0300

    connect to mysql

--- ask/nginx_init.sh
+++
@@ -1,3 +0,0 @@
-sudo rm -rf /etc/nginx/enabled-sites/default
-sudo ln -s /home/box/web/nginx_conf.conf /etc/nginx/sites-enabled/default
- 
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,14 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
 class Question(models.Model):
-    title = models.CharField(max_length=255)
+    objects = QuestionManager()
+    title = models.CharField(max_length=100)
     text = models.TextField()
-    added_at = models.DateField(blank=True)
-    rating = models.IntegerField()
-    author = models.ManyToManyField(User)
-    likes = User
+    added_at = models.DateField(blank=True, auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.OneToOneField(User)
+    likes = models.ManyToManyField(User, related_name='likes_set')
 
     def __unicode__(self):
         return self.title
